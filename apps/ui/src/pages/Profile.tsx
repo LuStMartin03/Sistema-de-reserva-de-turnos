a5cb2b08fdee2d697ca3ac90ab58ad1d4b7e661e
LuStMartin03 <lucia.st.martin@gmail.com>
2026-01-08 02:53:41 -0300

    aaaa

--- apps/ui/src/pages/Profile.tsx
+++ apps/ui/src/pages/Profile.tsx
@@ -4,7 +4,7 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 
 export default function Profile() {
-  const { profile, futureBooking, pastBookings, cancelBooking, deleteAccount,} = useUser();
+  const { profile, futureBooking, pastBookings, cancelBooking, deleteAccount} = useUser();
 
   async function handleDelete() {
     if (confirm("¿Seguro que querés eliminar tu cuenta?")) {
